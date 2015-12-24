4b7cf704891cc1591753be6d0a706f7e7922915e
biezhi <biezhi.me@gmail.com>
2015-12-24 17:15:40 +0800

    -

--- README.md
+++ README.md
@@ -4,16 +4,27 @@ This project is Java8 implementation of HTTP requests
 
 # Usage
 
-## 1. send get request 
+## 1. Get Request
 
 ```java
 String body = get("https://github.com/opensearch.xml").body();
 System.out.println(body);
 ```
 
-## 2. get url save to file
+## 2. Save To File
 
 ```java
 get("https://avatars3.githubusercontent.com/u/3849072?v=3&s=460").saveAsDisk(new File("D:/avatar.png"));
 ```
 
+## 3. Post Request
+
+```java
+post("http://xxxx.com").param("name", "jack").body();
+```
+
+## 4. Headers
+
+```java
+get("http://xxxx.com").header(Header.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; WOW64) xxxx").body();
+```
--- src/test/java/me/biezhi/request/test/AppTest.java
+++ src/test/java/me/biezhi/request/test/AppTest.java
@@ -1,18 +1,40 @@
 package me.biezhi.request.test;
 
-import static me.biezhi.request.Request.get;
+import static me.biezhi.request.Request.*;
 
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.Test;
+
 public class AppTest {
 
-	public static void main(String[] args) throws IOException {
-		
-//		String body = get("https://github.com/opensearch.xml").body();
-//		System.out.println(body);
-		
-		get("https://avatars3.githubusercontent.com/u/3849072?v=3&s=460").saveAsDisk(new File("D:/aaa.png"));
-		
+	@Test
+	public void testGet(){
+		try {
+			String body = get("https://github.com/opensearch.xml").body();
+			System.out.println(body);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
+	@Test
+	public void testSaveAsFile(){
+		try {
+			get("https://avatars3.githubusercontent.com/u/3849072?v=3&s=460").saveAsDisk(new File("D:/aaa.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testPost(){
+		try {
+			post("https://github.com/biezhi/java8-request").param("name", "jack").body();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
